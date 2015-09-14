b3674d126663a081603e4936ccb80c4b4a63a078
Duyet <duyet.nguyen@outlook.com>
2015-09-14 17:58:20 +0700

    link to show a recipe using redis

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,17 +1,27 @@
 class RecipesController < ApplicationController
-  before_action :load_recipe, except: [:index, :new]
+  before_action :load_recipe, except: [:index, :new, :show]
   before_action :create_database, only: [:index]
 
   def index
+    @count = counter
   end
 
   def show
+    @count = counter
+    @recipe = ""
+    all_recipes = $redis.smembers("recipes")
+    all_recipes.each do |recipe|
+      if params[:id] == $redis.hget(recipe, "id")
+        @recipe = recipe
+      end
+    end
   end
 
   def new
-    $redis.hmset("recipe:test", :title, "test", :summary, "Demo Description")
+    @count = counter
+    $redis.hmset("recipe:test",:id, 6, :title, "test", :summary, "Demo Description")
     $redis.sadd("recipes", "recipe:test")
-    redirect_to recipes_path
+    # redirect_to recipes_path
   end
 
   private
@@ -19,28 +29,34 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find_by params[:id]
   end
 
+  # Create counter visited
+  def counter
+    $redis.incr(:counter)
+  end
+
   def create_database
     # Create chefs
-    $redis.hmset("chef:nguyen", :name, "Nguyen Huynh", :email, "nguyen@gmail.com")
-    $redis.hmset("chef:nam", :name, "Nam Nguyen", :email, "nam@gmail.com")
-    $redis.hmset("chef:hoang", :name, "Hoang Nguyen", :email, "hoang@gmail.com")
+    $redis.hmset("chef:nguyen",:id, 1, :name, "Nguyen Huynh", :email, "nguyen@gmail.com")
+    $redis.hmset("chef:nam", :id, 2, :name, "Nam Nguyen", :email, "nam@gmail.com")
+    $redis.hmset("chef:hoang", :id, 3, :name, "Hoang Nguyen", :email, "hoang@gmail.com")
 
     $redis.sadd("chefs", "chef:nguyen")
     $redis.sadd("chefs", "chef:nam")
     $redis.sadd("chefs", "chef:hoang")
 
     # Create recipes
-    $redis.hmset("recipe:flare", :title, "Control Flare", :summary, " These flame flare-ups are every griller's frenemy")
-    $redis.hmset("recipe:peel", :title, "Peel Garlic", :summary, " Three Ways to Peel Garlic")
-    $redis.hmset("recipe:smokerless", :title, "Smoked Chicken", :summary, "you don't even need a smoker to make it")
-    $redis.hmset("recipe:grill", :title, "Grill So Food", :summary, "Season Your Grill So Food Won't Stick")
-    $redis.hmset("recipe:rib", :title, "Amazing Ribs", :summary, "Amazing Ribs with Meathead, Part I: Traditional Barbecue Techniques")
+    $redis.hmset("recipe:flare",:id, 1, :title, "Control Flare", :summary, " These flame flare-ups are every griller's frenemy")
+    $redis.hmset("recipe:peel",:id, 2, :title, "Peel Garlic", :summary, " Three Ways to Peel Garlic")
+    $redis.hmset("recipe:smokerless", :id, 3, :title, "Smoked Chicken", :summary, "you don't even need a smoker to make it")
+    $redis.hmset("recipe:grill",:id, 4, :title, "Grill So Food", :summary, "Season Your Grill So Food Won't Stick")
+    $redis.hmset("recipe:rib",:id, 5, :title, "Amazing Ribs", :summary, "Amazing Ribs with Meathead, Part I: Traditional Barbecue Techniques")
 
-    $redis.sadd("recipes", "recipe:flare")
+
     $redis.sadd("recipes", "recipe:peel")
     $redis.sadd("recipes", "recipe:smokerless")
     $redis.sadd("recipes", "recipe:grill")
     $redis.sadd("recipes", "recipe:rib")
+    $redis.sadd("recipes", "recipe:flare")
 
     # Create relationship
     $redis.sadd("chef:nguyen:recipes", "flare")
