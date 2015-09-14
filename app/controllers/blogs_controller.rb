a001a30ad6106fd4cf899e88da4a9a4226bf2969
Duyet <duyet.nguyen@outlook.com>
2015-09-14 14:24:34 +0700

    using redis to create db and set relationships

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,39 @@
+class BlogsController < ApplicationController
+  def index
+    # Create chefs
+    $redis.hmset("chef:nguyen", :name, "Nguyen Huynh", :email, "nguyen@gmail.com")
+    $redis.hmset("chef:nam", :name, "Nam Nguyen", :email, "nam@gmail.com")
+    $redis.hmset("chef:hoang", :name, "Hoang Nguyen", :email, "hoang@gmail.com")
+
+    $redis.sadd("chefs", "chef:nguyen")
+    $redis.sadd("chefs", "chef:nam")
+    $redis.sadd("chefs", "chef:hoang")
+
+    # Create recipes
+    $redis.hmset("recipe:flare", :title, "Control Flare", :summary, " These flame flare-ups are every griller's frenemy")
+    $redis.hmset("recipe:peel", :title, "Peel Garlic", :summary, " Three Ways to Peel Garlic")
+    $redis.hmset("recipe:smokerless", :title, "Smoked Chicken", :summary, "you don't even need a smoker to make it")
+    $redis.hmset("recipe:grill", :title, "Grill So Food", :summary, "Season Your Grill So Food Won't Stick")
+    $redis.hmset("recipe:rib", :title, "Amazing Ribs", :summary, "Amazing Ribs with Meathead, Part I: Traditional Barbecue Techniques")
+
+    $redis.sadd("recipes", "recipe:flare")
+    $redis.sadd("recipes", "recipe:peel")
+    $redis.sadd("recipes", "recipe:smokerless")
+    $redis.sadd("recipes", "recipe:grill")
+    $redis.sadd("recipes", "recipe:rib")
+
+    # Create relationship
+    $redis.sadd("chef:nguyen:recipes", "flare")
+    $redis.sadd("chef:nguyen:recipes", "peel")
+    $redis.sadd("chef:nguyen:recipes", "smokerless")
+    $redis.sadd("chef:nam:recipes", "grill")
+    $redis.sadd("chef:nam:recipes", "rib")
+
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
