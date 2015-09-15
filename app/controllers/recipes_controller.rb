e3b6e31260fb6ba60f2da2ce53a97d8deca4a51e
Duyet <duyet.nguyen@outlook.com>
2015-09-15 13:05:40 +0700

    function new create, edit, updating

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,27 +1,49 @@
 class RecipesController < ApplicationController
-  before_action :load_recipe, except: [:index, :new, :show]
+  before_action :load_recipe, except: [:index, :new, :show, :edit]
   before_action :create_database, only: [:index]
 
   def index
     @count = counter
+    @chefs = $redis.smembers("chefs")
   end
 
   def show
     @count = counter
-    @recipe = ""
-    all_recipes = $redis.smembers("recipes")
-    all_recipes.each do |recipe|
-      if params[:id] == $redis.hget(recipe, "id")
-        @recipe = recipe
-      end
-    end
+    @recipe = "recipe:#{params[:id]}"
   end
 
   def new
     @count = counter
-    $redis.hmset("recipe:test",:id, 6, :title, "test", :summary, "Demo Description")
-    $redis.sadd("recipes", "recipe:test")
-    # redirect_to recipes_path
+    @recipe = Recipe.new
+  end
+
+  def create
+    recipe_id = $redis.scard("recipes")
+    $redis.set("id", recipe_id)
+    id = $redis.incr(:id)
+    title = params[:recipe][:title]
+    summary = params[:recipe][:summary]
+
+    $redis.hmset("recipe:#{id}", :title, title, :summary, summary)
+    $redis.sadd("recipes", "recipe:#{id}")
+    redirect_to recipes_path
+  end
+
+  def edit
+    @count = counter
+    @recipe = "recipe:#{params[:id]}"
+  end
+
+  def update
+    byebug
+    id = params[:id]
+    byebug
+    title = params[:recipe][:title]
+    summary = params[:recipe][:summary]
+
+    $redis.hmset("recipe:#{id}", :title, title, :summary, summary)
+    $redis.sadd("recipes", "recipe:#{id}")
+    redirect_to recipe_path "recipe:#{id}"
   end
 
   private
@@ -36,33 +58,33 @@ class RecipesController < ApplicationController
 
   def create_database
     # Create chefs
-    $redis.hmset("chef:nguyen",:id, 1, :name, "Nguyen Huynh", :email, "nguyen@gmail.com")
-    $redis.hmset("chef:nam", :id, 2, :name, "Nam Nguyen", :email, "nam@gmail.com")
-    $redis.hmset("chef:hoang", :id, 3, :name, "Hoang Nguyen", :email, "hoang@gmail.com")
+    $redis.hmset("chef:1", :name, "Nguyen Huynh", :email, "nguyen@gmail.com")
+    $redis.hmset("chef:2", :name, "Nam Nguyen", :email, "nam@gmail.com")
+    $redis.hmset("chef:3", :name, "Hoang Nguyen", :email, "hoang@gmail.com")
 
-    $redis.sadd("chefs", "chef:nguyen")
-    $redis.sadd("chefs", "chef:nam")
-    $redis.sadd("chefs", "chef:hoang")
+    $redis.sadd("chefs", "chef:1")
+    $redis.sadd("chefs", "chef:2")
+    $redis.sadd("chefs", "chef:3")
 
     # Create recipes
-    $redis.hmset("recipe:flare",:id, 1, :title, "Control Flare", :summary, " These flame flare-ups are every griller's frenemy")
-    $redis.hmset("recipe:peel",:id, 2, :title, "Peel Garlic", :summary, " Three Ways to Peel Garlic")
-    $redis.hmset("recipe:smokerless", :id, 3, :title, "Smoked Chicken", :summary, "you don't even need a smoker to make it")
-    $redis.hmset("recipe:grill",:id, 4, :title, "Grill So Food", :summary, "Season Your Grill So Food Won't Stick")
-    $redis.hmset("recipe:rib",:id, 5, :title, "Amazing Ribs", :summary, "Amazing Ribs with Meathead, Part I: Traditional Barbecue Techniques")
+    $redis.hmset("recipe:1", :title, "Control Flare", :summary, " These flame flare-ups are every griller's frenemy")
+    $redis.hmset("recipe:2", :title, "Peel Garlic", :summary, " Three Ways to Peel Garlic")
+    $redis.hmset("recipe:3", :title, "Smoked Chicken", :summary, "you don't even need a smoker to make it")
+    $redis.hmset("recipe:4", :title, "Grill So Food", :summary, "Season Your Grill So Food Won't Stick")
+    $redis.hmset("recipe:5", :title, "Amazing Ribs", :summary, "Amazing Ribs with Meathead, Part I: Traditional Barbecue Techniques")
 
 
-    $redis.sadd("recipes", "recipe:peel")
-    $redis.sadd("recipes", "recipe:smokerless")
-    $redis.sadd("recipes", "recipe:grill")
-    $redis.sadd("recipes", "recipe:rib")
-    $redis.sadd("recipes", "recipe:flare")
+    $redis.sadd("recipes", "recipe:1")
+    $redis.sadd("recipes", "recipe:2")
+    $redis.sadd("recipes", "recipe:3")
+    $redis.sadd("recipes", "recipe:4")
+    $redis.sadd("recipes", "recipe:5")
 
     # Create relationship
-    $redis.sadd("chef:nguyen:recipes", "flare")
-    $redis.sadd("chef:nguyen:recipes", "peel")
-    $redis.sadd("chef:nguyen:recipes", "smokerless")
-    $redis.sadd("chef:nam:recipes", "grill")
-    $redis.sadd("chef:nam:recipes", "rib")
+    $redis.sadd("chef:1:recipes", "1")
+    $redis.sadd("chef:1:recipes", "2")
+    $redis.sadd("chef:1:recipes", "3")
+    $redis.sadd("chef:2:recipes", "4")
+    $redis.sadd("chef:2:recipes", "5")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914083258) do
+ActiveRecord::Schema.define(version: 20150915030201) do
 
   create_table "recipes", force: :cascade do |t|
+    t.string   "title"
+    t.string   "summary"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
